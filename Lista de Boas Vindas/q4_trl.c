c4873b4939379e191b010d3a927d4d8bc87acfc9
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-08-28 09:00:44 -0300

    Update q4_trl.c

--- Lista de Boas Vindas/q4_trl.c
+++ Lista de Boas Vindas/q4_trl.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h>
+ 
+int main() {
+  int x, i;
+  for (i = 0; i != 2002; i++){
+   printf("");
+   scanf("%d", &x);
+   if (x == 2002){
+    printf("Acesso Permitido\n");
+    break;
+   }
+   else{
+    printf("Senha Invalida\n");
+   }
+   }
+  return 0; 
+}
