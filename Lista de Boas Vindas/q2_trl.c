7f4ea2df10011be48ce03d43fe1796fcc3c17ab6
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-08-28 11:13:45 -0300

    Update q2_trl.c

--- Lista de Boas Vindas/q2_trl.c
+++ Lista de Boas Vindas/q2_trl.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main() {
+ float x1, x2, y1, y2;
+
+ scanf("%f %f", &x1, &y1);
+ scanf("%f %f", &x2, &y2);
+ printf("%.4f", sqrt(pow((x2-x1),2) + (pow((y2-y1),2))));
+ 
+  return 0;
+}
 
 /*Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
 Entrada
