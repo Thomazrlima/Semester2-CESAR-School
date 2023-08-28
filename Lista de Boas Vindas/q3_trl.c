6d4edfb17a1aabbfb10dc15df92d383674e1dc0a
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-08-28 09:16:29 -0300

    Update q3_trl.c

--- Lista de Boas Vindas/q3_trl.c
+++ Lista de Boas Vindas/q3_trl.c
@@ -22,3 +22,9 @@ int main(void) {
   printf("Em percentual: %.0f %%\n", (difx/x)*100);
   return 0;
 }
+
+// Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
+//Entrada
+//A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
+//Saída
+//Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste (ambos devem ser apresentados com 2 casas decimais) e o percentual de reajuste ganho, conforme exemplo abaixo.
