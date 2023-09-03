a34c9217a943eae5343c9c55ce6359d75f19d6a0
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-03 20:20:24 -0300

    Update q2_trl.c

--- Lista_1/q2_trl.c
+++ Lista_1/q2_trl.c
@@ -2,33 +2,38 @@
 
 int main(void) {
   int x, i, j;
-  int matriz[x][x];
   
   while(1){
     scanf("%d", &x);
+    
+    int matriz[x][x];
+    int y = 1;
 
     if (x == 0){
       break;
     }
 
     else{
-      
-      matriz[0][0] = 1;
-      for (i = 1; i <x; i++){
-        matriz[i][0] = matriz[i-1][0]*2;
-      }
-      
-      for (i = 0; i < x; i++){
-        for (j = 0; j < x; j++){
-          if (j != 0){
-            matriz[i][j] = matriz[i-1][j]*2;
-          }
-        }
-      }
 
+      matriz[0][0] = 1;
+      
+      for (i = 1; i < x; i++){
+        y = y*2;
+        matriz[i][0] = y;
+      }
+      
+      for (i = 0; i < x; i++){
+        for (j = 1; j < x; j++){
+          matriz[i][j] = matriz[i][j-1]*2;
+          }
+      }
+      
       for (i = 0; i < x; i++){
         for (j = 0; j < x; j++){
-          printf("  %d  ", matriz[i][j]);
+          printf("%*d", 3, matriz[i][j]);
+          if(j < x - 1){
+          printf(" ");
+        }
         }
         printf("\n");
       }
