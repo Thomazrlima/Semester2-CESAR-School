1676c87dc775bcdadc0186c0c2d3608084b2d415
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-04 10:51:05 -0300

    Create q2_trl.c

---
+++ Lista_1/q2_trl.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int G, A, M, C, i, j, k;
+  
+  scanf("%d %d %d %d", &G, &A, &M, &C);
+  
+  int matriza[G][A], matrizb[A][M], matrizc[M][C];
+
+  for (i = 0; i < G; i++){
+    for (j = 0; j < A; j++){
+       scanf("%d", &matriza[i][j]);
+      }
+  }
+  for (i = 0; i < A; i++){
+    for (j = 0; j < M; j++){
+       scanf("%d", &matrizb[i][j]);
+      }
+  }
+  for (i = 0; i < M; i++){
+    for (j = 0; j < C; j++){
+       scanf("%d", &matrizc[i][j]);
+      }
+  }
+
+  int result[G][M], fim[G][C];
+  
+  for (i = 0; i < G; i++){
+    for (j = 0; j < M; j++){
+      result[i][j] = 0;
+      for (k = 0; k < A; k++){
+        result[i][j] += matriza[i][k]*matrizb[k][j];
+      }
+    }
+  }
+  
+  for (i = 0; i < G; i++){
+    for (j = 0; j < C; j++){
+      fim[i][j] = 0;
+      for (k = 0; k < M; k++){
+        fim[i][j] += result[i][k]*matrizc[k][j];
+      }
+    }
+  }
+
+  for (i = 0; i < G; i++){
+    for (j = 0; j < C; j++){
+      printf("%*d", 3, fim[i][j]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
