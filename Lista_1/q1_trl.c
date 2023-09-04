25427b5ae43ce51a9e066a2d08ea5a8b27157f26
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-04 11:06:35 -0300

    Update q1_trl.c

--- Lista_1/q1_trl.c
+++ Lista_1/q1_trl.c
@@ -41,13 +41,3 @@ else if (A == B || B == C || A == C){
 }
 return 0;
 }
-
-/*Leia 3 valores de ponto flutuante A, B e C e ordene-os em ordem decrescente, de modo que o lado A representa o maior dos 3 lados. A seguir, determine o tipo de triângulo que estes três lados formam, com base nos seguintes casos, sempre escrevendo uma mensagem adequada:
-se A ≥ B+C, apresente a mensagem: NAO FORMA TRIANGULO
-se A2 = B2 + C2, apresente a mensagem: TRIANGULO RETANGULO
-se A2 > B2 + C2, apresente a mensagem: TRIANGULO OBTUSANGULO
-se A2 < B2 + C2, apresente a mensagem: TRIANGULO ACUTANGULO
-se os três lados forem iguais, apresente a mensagem: TRIANGULO EQUILATERO
-se apenas dois dos lados forem iguais, apresente a mensagem: TRIANGULO ISOSCELES
-Entrada
-A entrada contem três valores de ponto flutuante de dupla precisão A (0 < A) , B (0 < B) e C (0 < C).*/
