d2157c5cc869f431924259f46d09f15bcefd6bb2
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-11 15:52:44 -0300

    Update q3_trl.c

--- Lista_2/q3_trl.c
+++ Lista_2/q3_trl.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int comp(int Al, int Be, int A[Al], int B[Be]);
+
+int main(void) {
+  while(1){
+    int Al, Be, j, A[Al], B[Be];
+      scanf("%d %d", &Al, &Be);
+      if (Al == 0 && Be == 0 &&){
+        break;
+      }
+      for(j = 0; j < Al; j++){
+        scanf("%d", &A[i]);
+      }
+      for(j = 0; j < Al; j++){
+        scanf("%d", &A[i]);
+      }
+      comp(Al, Be, A, B);
+  }
+  return 0;
+}
+
+int comp(int Al, int Be){
+  int i, k, maior = Al;
+  if (Al < Be){
+    val = Be
+  }
+  for(i = 0; i < val; i++){
+      
+  }
+}
