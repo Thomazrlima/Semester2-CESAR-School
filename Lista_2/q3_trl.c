83c66c9f0121704d34b2a1d32c6ad4fd115638ba
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-12 18:34:30 -0300

    Update q3_trl.c

--- Lista_2/q3_trl.c
+++ Lista_2/q3_trl.c
@@ -4,11 +4,11 @@ int comp(int A, int B, int cardA[A], int cardB[B]);
 
 int main(void) {
   while(1){
-    int A, B, j;
+    int j, A, B;
       scanf("%d %d\n", &A, &B);
     
     int cardA[A], cardB[B];
-      if (A == 0 && B == 0){
+      if (A == 0 || B == 0){
         break;
       }
     
@@ -29,37 +29,26 @@ int comp(int A, int B, int cardA[A], int cardB[B]){
   int i, k, novoA[A], novoB[B], tama = 0, tamb = 0;
 
   novoA[0] = cardA[0];
-  for(i = 1; i < A; i++){
+  for(i = 1; i <= A; i++){
     if(cardA[i] != cardA[i-1]){
       tama += 1;
-      printf("carta diferente %d e %d\n", cardA[i], cardA[i-1]);
       novoA[tama] = cardA[i];
     }
   }
 
   novoB[0] = cardB[0];
-  for(i = 1; i < B; i++){
+  for(i = 1; i <= B; i++){
     if((cardB[i] != cardB[i-1])){
       tamb += 1;
-      printf("carta diferente %d e %d\n", cardB[i], cardB[i-1]);
       novoB[tamb] = cardB[i];
     }
   }
-
-  for(i = 0; i < A; i++){
-    printf("%d ", novoA[i]);
-  }
-  printf("\n");
-  for(i = 0; i < B; i++){
-    printf("%d ", novoB[i]);
-  }
-  printf("\n");
 
   int cont = 1, troca = 0;
   if(tama > tamb){
     for(i = 0; i < tamb; i++){
       for(k = 0; k < tama; k++){
-        if (novoA[i] == novoB[k]){
+        if (novoA[k] == novoB[i]){
           cont = 0;
         }
       }
@@ -70,7 +59,7 @@ int comp(int A, int B, int cardA[A], int cardB[B]){
   else{
     for(i = 0; i < tama; i++){
       for(k = 0; k < tamb; k++){
-        if (novoB[i] == novoA[k]){
+        if (novoB[k] == novoA[i]){
           cont = 0;
         }
       }
