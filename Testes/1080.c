7bbf84aa26d40132fe9a58e68ee572264f48664b
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-09-14 17:55:27 -0300

    Create 1080.c

---
+++ Testes/1080.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  int vetor[100], i, maior, lugar;
+  
+  for(i = 0; i < 100; i++){
+    scanf("%d", &vetor[i]);
+  }
+  for(i = 1; i < 100; i++){
+    if(vetor[i] > vetor[i-1]){
+      maior = vetor[i];
+      lugar = i;
+    }
+  }
+  printf("%d \n %d", maior, lugar);
+  return 0;
+}
