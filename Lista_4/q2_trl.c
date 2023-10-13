b50e6783336a47cef56082e96e6be8e9472dc727
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-10-13 11:30:40 -0300

    Update q2_trl.c

--- Lista_4/q2_trl.c
+++ Lista_4/q2_trl.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+struct van{
+  char nome[50];
+  char reg;
+  int dist;
+};
+
+int main(void) {
+
+  int casos, i, k;
+  struct van *alunos;
+  while (scanf("%d", &casos) != EOF){
+  
+    alunos = (struct van *)malloc(casos * sizeof(struct van));
+    
+    for(i = 0; i < casos; i++){
+      scanf("%s %c %d", alunos[i].nome, &alunos[i].reg, &alunos[i].dist); 
+    }
+    for (i = 0; i < casos; i++) {
+      for (k = i + 1; k < casos; k++) {
+        if (alunos[i].reg > alunos[k].reg) {
+          struct van aux;
+          aux = alunos[i];
+          alunos[i] = alunos[k];
+          alunos[k] = aux;
+        }
+      }
+    }
+  
+    for(i = 0; i < casos; i++){
+      for(k = i+1; k < casos; k++){
+        if(alunos[i].dist > alunos[k].dist){
+          struct van aux1 = alunos[i];
+          alunos[i] = alunos[k];
+          alunos[k] = aux1;
+        }
+      }
+    }
+  
+  
+    for(i = 0; i < casos; i++){
+      printf("%s\n", alunos[i].nome);
+    }
+    free(alunos);
+  }
+  
+  return 0;
+}
