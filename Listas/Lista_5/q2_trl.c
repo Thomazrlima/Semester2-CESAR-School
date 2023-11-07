8ef8e92d1f342b62c423cb7929a60eff47fce7c0
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-11-07 20:32:05 -0300

    Dor, apenas dor

--- Listas/Lista_5/q2_trl.c
+++ Listas/Lista_5/q2_trl.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct node{
+struct node {
   char nome[21];
   struct node *next;
 };
 
 void printList(struct node *head);
 void adicionar(struct node **head, char *nome);
-void deletar(struct node *head, char *nome);
+void deletar(struct node **head, char *nome);
 
 int main(void) {
   int N, i;
@@ -18,65 +18,55 @@ int main(void) {
 
   scanf("%d", &N);
 
-  for(i=0; i < N; i++){
+  for (i = 0; i < N; i++) {
     scanf("%s", nome);
     adicionar(&head, nome);
   }
-  
-  deletar(&head, *nome);
+
+  deletar(&head, nome);
   printList(head);
 
   return 0;
 }
 
-void adicionar(struct node **head, char *nome){
+void adicionar(struct node **head, char *nome) {
   struct node *n = *head;
   struct node *novo = (struct node *)malloc(sizeof(struct node));
-  strcpy(novo->nome,nome);
+  strcpy(novo->nome, nome);
 
-  if(*head == NULL){
-     *head = novo;
-     novo->next = NULL;
-  }else{
-
-    if(strcmp(novo->nome, n->nome) < 0){
-      novo->next = n;
-      *head = novo;
-    }
-    else{
-    while(n->next != NULL && strcmp(novo->nome, nome) < 0){
+  if (*head == NULL || strcmp(novo->nome, n->nome) < 0) {
+    novo->next = *head;
+    *head = novo;
+  } else {
+    while (n->next != NULL && strcmp(novo->nome, n->next->nome) >= 0) {
       n = n->next;
-        }
     }
-    if(n->next == NULL){
-      novo->next = NULL;
-      n->next = novo;  
-    }else{
-      novo->next = n->next;
-      n->next = novo;
-     } 
+
+    novo->next = n->next;
+    n->next = novo;
   }
 }
 
-void printList(struct node *head){
+void printList(struct node *head) {
   struct node *n = head;
-  while(n!= NULL){
-    printf(" %s ",n->nome);
-    n = n -> next;
+  while (n != NULL) {
+    printf(" %s ", n->nome);
+    n = n->next;
   }
   printf("\n");
 }
 
-void deletar(struct node *head, char *nome){
-  struct node *n = head;
+void deletar(struct node **head, char *nome) {
+  struct node *n = *head;
   struct node *temp;
-  struct node *novo = (struct node *)malloc(sizeof(struct node));
 
-  while(n->next->next != NULL && (strcmp(novo->nome, n->next->nome) == 0)){
-    n = n->next;
+  while (n != NULL && n->next != NULL) {
+    if (strcmp(n->next->nome, nome) == 0) {
+      temp = n->next;
+      n->next = n->next->next;
+      free(temp);
+    } else {
+      n = n->next;
+    }
   }
-
-  temp = n->next; 
-  n->next = n->next->next;
-  free(temp);
 }
