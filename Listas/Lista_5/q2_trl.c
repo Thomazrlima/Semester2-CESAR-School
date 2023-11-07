6494f9fd7f2d72a6aa639c0cd5fb35d3d0bb99bc
Thomaz Lima <126795323+Thomazrlima@users.noreply.github.com>
2023-11-07 11:58:44 -0300

    2729 v3

--- Listas/Lista_5/q2_trl.c
+++ Listas/Lista_5/q2_trl.c
@@ -7,45 +7,56 @@ struct node{
   struct node *next;
 };
 
-void adicionar(struct node **head, char nome, int i;
-
+void printList(struct node *head);
+void adicionar(struct node **head, char *nome);
 
 int main(void) {
   int N, i;
   char nome[21];
   struct node *head = NULL;
-  
+
   scanf("%d", &N);
 
   for(i=0; i < N; i++){
-      adicionar(&head,scanf("%s", nome), N);
+    scanf("%s", nome);
+    adicionar(&head, nome);
   }
+  printList(head);
 
-  struct node *n = head;
-  while(n!= NULL){
-  if(strcmp(n->nome, (n+1)->nome) > 0){
-    char aux1 = nome[i];
-    nome[i] = nome[i+1];
-    nome[i+1] = aux1;
-    }
-  }
+  return 0;
 }
 
-void add_node(struct node **head, char nome,int i){
-  if(i == 0){
-    *head = (struct node *) malloc(sizeof(struct node));
-    (*head)->nome = nome;
-    (*head)->next = NULL;
+void adicionar(struct node **head, char *nome){
+  struct node *n = *head;
+  struct node *novo = (struct node *)malloc(sizeof(struct node));
+  strcpy(novo->nome,nome);
 
+  if(*head == NULL){
+     *head = novo;
+     novo->next = NULL;
   }else{
-    struct node *n = *head;
-    while(n->next != NULL){
-    n = n->next;
+    while(n->next != NULL && strcmp(novo->nome, nome) < 0){
+        if (strcmp(novo->nome, nome) == 0){
+            free(novo);
+        }else{
+          n = n->next;
+        }
+    }
+    if(n->next == NULL){
+      novo->next = NULL;
+      n->next = novo;  
+    }else{
+      novo->next = n->next;
+      n->next = novo;
+     } 
    }
+}
 
-  n->next = (struct node *) malloc(sizeof(struct node));
-  n->next->nome = nome;
-  n->next->next = NULL;
+void printList(struct node *head){
+  struct node *n = head;
+  while(n!= NULL){
+    printf(" %s ",n->nome);
+    n = n -> next;
   }
-
+  printf("\n");
 }
